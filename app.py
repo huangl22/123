d8ea5585237f5e2d5ae2700733a1871b419b7906
huangl22 <112144251+huangl22@users.noreply.github.com>
2022-10-25 10:11:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
-from flask import Flask, redirect, url_for, request, render_template, make_response, session
-import config
-import os
-
+from flask import Flask, request, render_template
+import subprocess
+import shlex
 app = Flask(__name__)
-app.config.from_object(config)
-app.secret_key = 'fkdjsafjdkfdlkjfadskjfadskljdsfklj'
+
 @app.route('/')
-# def index():
-#     return render_template
 def index():
     return render_template('index_1.html')
 
-@app.route('/action', methods = ['POST'])
+
+@app.route('/action', methods=['POST'])
 def do_action():
     ueip = request.form['ueip']
     teid = request.form['teid']
     address = request.form['address']
-    dictionary = {"ueip":ueip, "teid":teid, "address":address}
+    dictionary = {"ueip": ueip, "teid": teid, "address": address}
+    print(dictionary)
 
     if ueip == "60.60.0.2" and teid == "3":
-        outstr1 = "table_add tognb gtpu_encap " + ueip + " => " + int(teid) + " 11.11.11.1 11.11.11.231\n"
-        os.popen(outstr1)
-        outstr2 = "table_add toupf gtpu_decap 11.11.11.231 11.11.11.1 " + int(teid)*2-1 + " " + ueip + " =>\n"
-        os.popen(outstr2)
-        outstr3 = "table_add forarp arp_response " + ueip + " => " + ueip + "\n"
-        os.popen(outstr3)
-if __name__== "__main__":
+        # outstr1 = "table_add tognb gtpu_encap " + ueip + " => " + teid + " 11.11.11.1 11.11.11.231\n"
+        process.stdin.write(f"table_add tognb gtpu_encap {ueip} => {teid} 11.11.11.1 11.11.11.231\n".encode())
+        process.stdin.flush()
+        hint = process.stdout.readline().decode()
+        print(hint)
+
+        # outstr2 = "table_add toupf gtpu_decap 11.11.11.231 11.11.11.1 " + str(int(teid) * 2 - 1) + " " + ueip + " =>\n"
+        teid = str(int(teid) * 2 - 1)
+        process.stdin.write(f"table_add toupf gtpu_decap 11.11.11.231 11.11.11.1 {teid} {ueip} =>\n".encode())
+        process.stdin.flush()
+        hint = process.stdout.readline().decode()
+        print(hint)
+
+        # outstr3 = "table_add forarp arp_response " + ueip + " => " + ueip + "\n"
+        process.stdin.write(f"table_add forarp arp_response {ueip} => {ueip}\n".encode())
+        process.stdin.flush()
+        hint = process.stdout.readline().decode()
+        print(hint)
+        return "success"
+    return "drop"
+
+if __name__ == "__main__":
+    process = subprocess.Popen(shlex.split("python test.py"), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     app.run(debug=True)
